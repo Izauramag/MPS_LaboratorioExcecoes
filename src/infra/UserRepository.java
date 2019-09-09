65084fa6604dcb8b7972bc2db8685e085a073fb1
Caio Nascimento <caionnc@gmail.com>
2019-09-09 20:38:24 -0300

    Create UserRepository.java

---
+++ src/infra/UserRepository.java
@@ -0,0 +1,56 @@
+package infra;
+
+import java.util.HashMap;
+import java.util.Set;
+
+import business.model.User;
+import exceptions.PersistOperationException;
+import exceptions.UserAlreadyAddedException;
+import exceptions.UserNotFoundException;
+
+public class UserRepository implements RepositoryPersistence
+{
+    private final HashMap<String, User> data;
+    private final RepositoryPersistence persistence;
+    
+    UserRepository(HashMap<String, User> data, RepositoryPersistence persistence)
+    {
+    	this.data = data;
+    	this.persistence = persistence;
+    }
+    
+    public void add(User user) throws UserAlreadyAddedException
+    {
+        if (this.data.containsKey(user.getLogin()))
+        {
+        	user = this.data.get(user.getLogin());
+        	System.out.println("== User exists: " + user.getLogin() + ", " + user.getPassword());
+        	throw new UserAlreadyAddedException();
+        }
+        
+        this.data.put(user.getLogin(), user);
+    }        
+        
+    public void delete(String login) throws UserNotExistException
+    {
+        if (this.data.remove(login) == null)
+            throw new UserNotExistException();
+    }
+    
+    public User get(String login) throws UserNotExistException
+    {
+    	if (this.data.containsKey(login))
+    		return this.data.get(login);
+    	else
+            throw new UserNotExistException();
+    }
+    
+    public Set<String> list() {
+    	return this.data.keySet();
+    }
+    
+    @Override
+    public void persist() throws PersistOperationException {
+    	this.persistence.persist();
+    }
+}
