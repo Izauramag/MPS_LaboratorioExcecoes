380e70a4efe1dbdaed69efb961a1343e7af3a181
Izaura Magalhães <izauradangela@gmail.com>
2019-09-09 20:54:00 -0300

    Create FileInputAdapter.java

---
+++ src/util/FileInputAdapter.java
@@ -0,0 +1,26 @@
+package util;
+
+import java.io.Closeable;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
+public class FileInputAdapter implements Closeable
+{
+	private ObjectInputStream in;
+	
+	public FileInputAdapter(String fileName) throws IOException
+	{
+		FileInputStream fin = new FileInputStream(fileName);
+	    this.in = new ObjectInputStream(fin);
+	}
+	
+	public Object read() throws ClassNotFoundException, IOException {
+		return this.in.readObject();
+	}
+
+	@Override
+	public void close() throws IOException {
+		this.in.close();
+	}
+}
